7bfcd3a1c04ab91bc8ab16c743025a9e011e5761
Matteo Giaccone <matteo@turnintocoders.it>
2018-02-19 15:33:07 +0100

    Small fix and refactor

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
 var getGif = function(textForm) {
   var xhr = new XMLHttpRequest();
   var url = "http://api.giphy.com/v1/gifs/search?q=" + textForm + "&api_key=dc6zaTOxFJmzC";
-  console.log(url);
 
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
       var giphyObj = JSON.parse(xhr.responseText);
-      console.log(giphyObj);
       var newImage;
-      var link = "";
-      var gallery;
-      var newGallery;
-      document.getElementById("gallery").value = "";
-      for (var i = 1; i < giphyObj.data.length; i++) {
-        //var gifDrop = document.querySelector(".gif");
-        link = giphyObj.data[i].images.downsized_medium.url;
+      var gallery = document.getElementById("gallery");
+      gallery.innerHTML = "";
+      for (var i = 0; i < giphyObj.data.length; i++) {
         newImage = document.createElement("img");
-        newImage.src = link;
-        console.log(newImage);
-        newGallery = document.getElementById("search-results").appendChild(newImage);
+        newImage.src = giphyObj.data[i].images.downsized_medium.url;
+        gallery.appendChild(newImage);
       }
-      document.getElementById("gallery").replaceChild(gallery, newGallery);
     }
   };
 
@@ -30,8 +22,7 @@ var getGif = function(textForm) {
 
 document.getElementById("searchForm").addEventListener("submit", function(event) {
   event.preventDefault();
-  var textForm = event.target.querySelector("input").value;
-  console.log(textForm);
-  getGif(textForm);
-  event.target.querySelector("input").value = "";
+  var inputField = event.target.querySelector("input");
+  getGif(inputField.value);
+  inputField.value = "";
 });
